00b2a69411a5733dbf824dcbf28aa0097f2880b6
iz <43661631+mencretsu@users.noreply.github.com>
2026-05-24 21:20:06 +0700

    Update beras.py

--- beras.py
+++ beras.py
@@ -76,10 +76,9 @@ is_today = tgl_today == now.strftime("%Y-%m-%d")
 tgl_label = now.strftime("%-d %b %Y") if is_today else \
             f"data per {datetime.strptime(tgl_today, '%Y-%m-%d').strftime('%-d %b')}"
 
-msg = "\n".join(lines) + f"\n\n{tgl_label} \n· Data by: sp2kp.kemendag.go.id"
 msg = "\n".join(lines) + \
-      f"\n\n<i>data diambil berdasarkan harga nasional tertimbang (HNT) pasar tradisional</i>" \
-      f"\n{tgl_label} \n· Data by: sp2kp.kemendag.go.id"
+      f"\n\n<i>ℹ️ data diambil berdasarkan harga nasional tertimbang (HNT) pasar tradisional</i>" \
+      f"\n\n{tgl_label} \nPrice by: sp2kp.kemendag.go.id"
 
 # print(msg)
 # print(f"TOKEN: {BOT_TOKEN[:10]}...")
